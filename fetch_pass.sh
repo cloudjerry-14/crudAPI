923694234ff66e327732fdf08724d9dc78b0fa92
Ashutosh Parihar <ashutoshparihar0@gmail.com>
2023-10-28 21:23:37 +0000

    fect at root level

--- fetch_pass.sh
+++ fetch_pass.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 # Obtain HCP_API_TOKEN
 HCP_API_TOKEN=$(curl --location 'https://auth.hashicorp.com/oauth/token' \
   --header 'content-type: application/json' \
@@ -10,7 +12,7 @@ HCP_API_TOKEN=$(curl --location 'https://auth.hashicorp.com/oauth/token' \
     "client_secret": "'$HCP_CLIENT_SECRET'"
   }' | jq -r .access_token)
 
-# Retrieve DB_PASS from HashiCorp Vault
+# Retrieve DB_PASS from HashiCorp Vault and export the variable
 DB_PASS=$(curl --location "https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/9975bd6f-ca79-4d0b-bd7a-d38e4bc01396/projects/1d0f38e2-c541-4756-86bc-6575c66deecc/apps/EmployeeApp/open" \
   --request GET \
   --header "Authorization: Bearer $HCP_API_TOKEN" | jq -r '.secrets[0]')
