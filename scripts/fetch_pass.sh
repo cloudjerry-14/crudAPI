7e2de195dfb47f852253f0a08836dd0e1ce2d4fa
Ashutosh Parihar <ashutoshparihar0@gmail.com>
2023-10-28 21:55:28 +0000

    pass

--- scripts/fetch_pass.sh
+++ scripts/fetch_pass.sh
@@ -15,7 +15,14 @@ HCP_API_TOKEN=$(curl --location 'https://auth.hashicorp.com/oauth/token' \
 # Retrieve DB_PASS from HashiCorp Vault and export the variable
 DB_PASS=$(curl --location "https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/9975bd6f-ca79-4d0b-bd7a-d38e4bc01396/projects/1d0f38e2-c541-4756-86bc-6575c66deecc/apps/EmployeeApp/open" \
   --request GET \
-  --header "Authorization: Bearer $HCP_API_TOKEN" | jq -r '.secrets[0]')
+  --header "Authorization: Bearer $HCP_API_TOKEN")
+
+# Assuming DB_PASS contains the JSON structure
+password=$(echo "$DB_PASS" | jq -r '.version.value')
+echo "Password extracted from JSON: $password"
+
+# Export the password as an environment variable
+export DB_PASSWORD=$password
 
 # Export the DB_PASS variable
 export DB_PASS
